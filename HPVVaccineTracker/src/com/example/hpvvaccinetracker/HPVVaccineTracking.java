8e207123f6ec2f8853741c7eb234131d7e3dbe49
Angela Liu <eeke.its.angela@gmail.com>
2013-05-25 22:57:54 -0700

    added the front-end app

---
+++ HPVVaccineTracker/src/com/example/hpvvaccinetracker/HPVVaccineTracking.java
@@ -0,0 +1,24 @@
+package com.example.hpvvaccinetracker;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class HPVVaccineTracking extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_hpvvaccine_tracking);
+		
+		// 
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.hpvvaccine_tracking, menu);
+		return true;
+	}
+
+}
